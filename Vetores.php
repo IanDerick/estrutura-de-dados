084e534a58f7c8543e28196fde2be624b1e1fa91
Ian Derick <ian_mota@estudante.sesisenai.org.br>
2026-02-12 21:04:06 -0300

    Fazendo segunda parte do primeiro exercicio

--- Vetores.php
+++ Vetores.php
@@ -11,6 +11,18 @@ function buscarNumero($vetor, $numeroProcurado) {
     return "Número NÂO encontrado no vetor.";
 }
 
-$numero = 18; 
+function removerNumero(&$vetor, $posicao) {
+    if (isset($vetor[$posicao])) {
+        unset($vetor[$posicao]);
+        $vetor = array_values($vetor);
+        return "Elemento removido com sucesso.";
+    } else {
+        return "Elemento não encontrado.";
+    }
+}
+
+$numero = 1; 
 echo buscarNumero($numeros, $numero);
+echo removerNumero($numeros, 3);
+print_r($numeros);
 ?>

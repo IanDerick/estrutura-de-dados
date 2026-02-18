159f6efdbd03a8ae19d5f87b56a183f17c69eff0
Ian Derick <ian_mota@estudante.sesisenai.org.br>
2026-02-18 20:53:14 -0300

    Fazendo excercicio de sequencia de fibonacci

--- Recursividade/sequencia.php
+++ Recursividade/sequencia.php
@@ -1,5 +1,36 @@
 <?php
 
+/*function fibonacci($n) { 
+    if ($n === 0) {
+        return 0;
+    }elseif ($n === 1) {
+        return 1;
+    }
+    return fibonacci($n - 1) + fibonacci($n - 2); 
+}
+echo fibonacci(20);
 
+?>*/
+
+
+
+function fibonacci($n, &$memo = []) {
+    if ($n < 0) {
+        throw new Exception("Número inválido");
+    }
+
+    if ($n === 0) return 0;
+    if ($n === 1) return 1;
+
+    if (isset($memo[$n])) {
+        return $memo[$n];
+    }
+
+    $memo[$n] = fibonacci($n - 1, $memo) + fibonacci($n - 2, $memo);
+
+    return $memo[$n];
+}
+
+echo fibonacci(200);
 
 ?>
